025459cfc5d08ee3ea9d3d36fbb89f7947dda7ed
kunxin-chor <chorkunxin@yahoo.com>
2020-11-20 07:02:06 +0000

    Added calculate BMI example

---
+++ lab6-forms/index.js
@@ -0,0 +1,77 @@
+// const means never changes - short for constant
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+
+// give me a new instance of an express application
+// the `app` variable shouldn't be changed
+const app = express();
+
+// setup the view engine
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+// setup wax on so that it will works with hbs
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts')
+
+// setup forms
+app.use(express.urlencoded({
+    extended: false
+}))
+
+// BEGIN ROUTES
+
+app.get('/food', (req,res)=>{
+    res.render('enter_food.hbs')
+})
+
+app.post('/food', (req,res)=>{
+    console.log(req.body);
+    // let tags = req.body.tags;
+    // if (! Array.isArray(tags)) {
+    //     tags = [ tags];
+    // }
+
+    // let tags = Array.isArray(req.body.tags) ? req.body.tags : [req.body.tags];
+    // console.log(tags);
+    // res.send(req.body);
+
+    // let name = req.body.name;
+    // let meal = req.body.meal;
+    // let tags = req.body.tags;
+    // ==>
+    let { food, meal, tags} = req.body;
+
+    // res.render('summary.hbs', {
+    //     'foodName': foodName,
+    //     'meal': meal,
+    //     'tags': tags
+    // })
+    // ==>
+    res.render('summary.hbs', {
+        food, meal, tags
+    })
+})
+
+// display the form
+app.get('/calculate_bmi', (req,res)=>{
+    res.render('calculate_bmi.hbs')
+})
+
+app.post('/calculate_bmi', (req,res)=>{
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    // or we can use destructuring
+    // let {weight height} = req.body;
+    let bmi = weight / (height * height);
+    res.render('bmi_results.hbs',{
+        'bmi': bmi
+    })
+})
+
+// END ROUTES
+
+app.listen(3000, ()=>{
+    console.log("Server has started");
+})
---
+++ lab6-forms/setup-express.sh
@@ -0,0 +1,5 @@
+npm init
+yarn add express
+yarn add hbs
+yarn add wax-on
+touch index.js
