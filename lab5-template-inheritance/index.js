0e3a4ef0d77a68c83ddda3fb43990d963da996e4
kunxin-chor <chorkunxin@yahoo.com>
2020-11-20 03:19:41 +0000

    Added template inheritance and revision example

---
+++ lab5-template-inheritance/index.js
@@ -0,0 +1,34 @@
+// const means never changes - short for constant
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+
+// give me a new instance of an express application
+// the `app` variable shouldn't be changed
+const app = express();
+
+// setup the view engine
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+// setup wax on so that it will works with hbs
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts')
+
+// end express setup
+app.get('/', (req,res)=>{
+    res.render('index.hbs');
+})
+
+app.get('/about-us', (req,res)=>{
+    res.render('about-us.hbs');
+})
+
+app.get('/contact-us', (req,res)=>{
+    res.render('contact-us.hbs')
+})
+
+
+app.listen(3000, ()=>{
+    console.log("Server started")
+})
